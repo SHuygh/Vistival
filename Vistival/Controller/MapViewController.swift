0dc2fadbf6417e889967f68c5737cf72657be99e
Daan Demeulemeester <Millmaster@localhos>
2018-01-29 13:31:11 +0100

    mapKit
    not tested

---
+++ Vistival/Controller/MapViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MapViewController.swift
+//  Vistival
+//
+//  Created by Daan Demeulemeester on 29/01/2018.
+//  Copyright © 2018 Stijn Huygh. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // coordinaten van het festival via klassen
+        
+        let center = CLLocationCoordinate2D(latitude: 30, longitude: 30)
+        let span = MKCoordinateSpanMake(2, 2)
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        mapView.region = region
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
