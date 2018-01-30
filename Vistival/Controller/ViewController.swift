4309e67042ac5aff96374ed0e8bbcdcee766554a
Daan Demeulemeester <Millmaster@localhos>
2018-01-30 11:39:56 +0100

    news page fix new view controller

--- Vistival/Controller/ViewController.swift
+++ Vistival/Controller/ViewController.swift
@@ -14,8 +14,11 @@ import AVFoundation
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     
+    @IBOutlet weak var tableView: UITableView!
     
-    let data = ImportData.data;
+    let data = ImportData.data
+    var newsItems = ImportData.data.newsList
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,15 +30,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        print("test")
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return newsItems.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        let currentItem:NewsFeed = newsItems[indexPath.row]
+        cell?.textLabel?.text = newsItems[indexPath.row].title
+        return cell!
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let destination:NewsItemsViewController = segue.destination as! NewsItemsViewController
+        
+        let indexpath = tableView.indexPath(for: sender as! UITableViewCell)
+        
+        let toPass:NewsFeed = newsItems[indexpath!.row]
+        
+        destination.currentItem = toPass
+        
     }
 
 }
