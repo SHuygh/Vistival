2bab3151e3c276b9b931522f7145f97677658d49
Daan Demeulemeester <Millmaster@localhos>
2018-02-01 16:27:50 +0100

    lolz, faq, news

---
+++ Vistival/Controller/FAQTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FAQTableViewCell.swift
+//  Vistival
+//
+//  Created by Daan Demeulemeester on 01/02/2018.
+//  Copyright © 2018 Stijn Huygh. All rights reserved.
+//
+
+import UIKit
+
+class FAQTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblTitle: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
